5ad3c55c34127518a24cc9c1cd2562706d802b09
silantyevmn <silantyevmn@yandex.ru>
2018-12-09 14:44:15 +0300

    Добавил bottonNavigationBar и обработка переключений между фрагментами Небольшие манипуляции с toolbar Вывод на экран ошибок через popupWindows

--- app/src/main/java/silantyevmn/ru/mynews/ui/activity/StartActivity.java
+++ app/src/main/java/silantyevmn/ru/mynews/ui/activity/StartActivity.java
@@ -1,12 +1,16 @@
 package silantyevmn.ru.mynews.ui.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
@@ -20,16 +24,18 @@ import ru.terrakok.cicerone.Router;
 import ru.terrakok.cicerone.android.support.SupportAppNavigator;
 import silantyevmn.ru.mynews.App;
 import silantyevmn.ru.mynews.R;
-import silantyevmn.ru.mynews.ui.popup.PopupDialogMessage;
 import silantyevmn.ru.mynews.presenter.StartPresenter;
 import silantyevmn.ru.mynews.ui.common.BackButtonListener;
+import silantyevmn.ru.mynews.ui.popup.PopupDialogMessage;
 import silantyevmn.ru.mynews.ui.view.StartView;
 
 public class StartActivity extends MvpAppCompatActivity implements StartView {
     private Toolbar toolbar;
+    private TextView titleToolbar;
+    private SearchView searchView;
 
     @Inject
-    PopupDialogMessage popupDialog;
+    PopupDialogMessage popupWindow;
 
     @Inject
     Router router;
@@ -45,36 +51,65 @@ public class StartActivity extends MvpAppCompatActivity implements StartView {
         return presenter;
     }
 
-    public Toolbar getToolbar() {
+    public void initToolbar(String title) {
+        toolbar.getMenu().findItem(R.id.search).setVisible(true);
+        toolbar.setNavigationIcon(null);
+        titleToolbar.setText(title);
+    }
+
+    public Toolbar getToolbar(){
         return toolbar;
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         App.getInstance().getComponent().inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
         toolbar = findViewById(R.id.toolbar);
+        titleToolbar = findViewById(R.id.toolbar_title);
         onCreateOptionsMenu(toolbar.getMenu());
 
         if (savedInstanceState == null) {
-            presenter.toMainScreen();
+            presenter.homeScreen(getString(R.string.title_home));
         }
+        BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case R.id.navigation_home:{
+                    presenter.homeScreen(getString(R.string.title_home));
+                    return true;
+                }
+                case R.id.navigation_category:{
+                    presenter.categoryScreen(getString(R.string.title_category));
+                    return true;
+                }
+                case R.id.navigation_bookmarks:{
+                    presenter.bookmarksScreen(getString(R.string.title_bookmarks));
+                    return true;
+                }
+            }
+            return false;
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_start, menu);
         MenuItem item = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView) item.getActionView();
+        searchView = (SearchView) item.getActionView();
         searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnSearchClickListener(view -> {
+            titleToolbar.setVisibility(View.INVISIBLE);
+            toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+            toolbar.setNavigationOnClickListener(navIconView -> closeSearch());
+        });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                //searchView.clearFocus();
                 presenter.searchNewsScreen(query);
-                //searchView.clearFocus();
                 return false;
             }
 
@@ -85,11 +120,17 @@ public class StartActivity extends MvpAppCompatActivity implements StartView {
         });
         searchView.setOnQueryTextFocusChangeListener((view1, hasFocus) -> {
             if (!hasFocus) {
-                //presenter.closeSearchFragment();
-                searchView.onActionViewCollapsed();
+                closeSearch();
             }
         });
         return true;
+    }
+
+    private void closeSearch() {
+        searchView.onActionViewCollapsed();
+        titleToolbar.setVisibility(View.VISIBLE);
+        toolbar.setNavigationIcon(null);
+        toolbar.setNavigationOnClickListener(null);
     }
 
     @Override
@@ -99,7 +140,7 @@ public class StartActivity extends MvpAppCompatActivity implements StartView {
 
     @Override
     public void showError(String errorTextMessage) {
-
+        popupWindow.error(getCurrentFocus(), errorTextMessage);
     }
 
     @Override
