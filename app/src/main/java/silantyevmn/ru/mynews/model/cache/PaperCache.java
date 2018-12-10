b47fa7dca0c12ddf819d2c57e707a6e3498ece37
silantyevmn <silantyevmn@yandex.ru>
2018-12-10 14:02:31 +0300

    реализация Cache getBookmarksList() updateBookmark(Articles) bool findBookmark(Articles)

--- app/src/main/java/silantyevmn/ru/mynews/model/cache/PaperCache.java
+++ app/src/main/java/silantyevmn/ru/mynews/model/cache/PaperCache.java
@@ -7,26 +7,54 @@ import java.util.List;
 import io.paperdb.Paper;
 import io.reactivex.Observable;
 import silantyevmn.ru.mynews.model.entity.Articles;
-import silantyevmn.ru.mynews.model.entity.News;
 
 
-public class PaperCache implements Cache{
-    private final String BASE_KEY="paper_key";
-
-    @Override
-    public void putAll(News news) {
-        Paper.book().write(BASE_KEY,news);
-    }
+public class PaperCache implements Cache {
+    private final String FAVORITES_KEY = "favorites_key";
 
     @Override
-    public Observable<News> getNews() {
-        return Observable.create(e->{
-            News news= Paper.book().read(BASE_KEY);
-            if(news==null){
-                news=new News("","",new ArrayList<>());
+    public Observable<List<Articles>> getBookmarksList() {
+        return Observable.create(emitter -> {
+            List<Articles> bookmarksList = Paper.book().read(FAVORITES_KEY);
+            if (bookmarksList == null) {
+                bookmarksList = new ArrayList<>();
             }
-            e.onNext(news);
-            e.onComplete();
+            emitter.onNext(bookmarksList);
+            emitter.onComplete();
         });
     }
+
+    @Override
+    public Observable<Boolean> updateBookmark(Articles articles) {
+        return Observable.create(emitter -> {
+            List<Articles> bookmarksList = Paper.book().read(FAVORITES_KEY);
+            if (bookmarksList == null) {
+                bookmarksList = new ArrayList<>();
+            }
+            if (bookmarksList.contains(articles)) {
+                bookmarksList.remove(articles);
+                emitter.onNext(false);
+            } else {
+                bookmarksList.add(articles);
+                emitter.onNext(true);
+            }
+            Paper.book().write(FAVORITES_KEY, bookmarksList);
+            emitter.onComplete();
+        });
+    }
+
+    @Override
+    public Observable<Boolean> findBookmark(Articles articles) {
+        return Observable.create(emitter -> {
+            List<Articles> bookmarksList = Paper.book().read(FAVORITES_KEY);
+            if (bookmarksList == null || bookmarksList.size() == 0) {
+                emitter.onNext(false);
+            } else {
+                emitter.onNext(bookmarksList.contains(articles));
+            }
+            emitter.onComplete();
+        });
+
+    }
+
 }
