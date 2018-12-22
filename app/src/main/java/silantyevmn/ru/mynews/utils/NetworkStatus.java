f705fffd6e718c1bfa36d3680145bf2f497eae52
silantyevmn <silantyevmn@yandex.ru>
2018-12-22 23:55:28 +0300

    fix

--- app/src/main/java/silantyevmn/ru/mynews/utils/NetworkStatus.java
+++ app/src/main/java/silantyevmn/ru/mynews/utils/NetworkStatus.java
@@ -2,17 +2,14 @@ package silantyevmn.ru.mynews.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.widget.Toast;
 
 import silantyevmn.ru.mynews.App;
-import silantyevmn.ru.mynews.R;
 
 public class NetworkStatus {
 
     public static boolean isInternetAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) App.getInstance().getSystemService(Context.CONNECTIVITY_SERVICE);
         return connectivityManager.getActiveNetworkInfo() != null
-                && connectivityManager.getActiveNetworkInfo().isAvailable()
                 && connectivityManager.getActiveNetworkInfo().isConnected();
     }
 }
